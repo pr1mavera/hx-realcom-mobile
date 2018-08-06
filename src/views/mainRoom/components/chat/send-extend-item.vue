47e32c6b888e756b77111534ca795514bbfa91bd
primavera <primaveraiiiiing618@gmail.com>
2018-08-06 22:05:05 +0800

    8/6 22:11 update

---
+++ src/views/mainRoom/components/chat/send-extend-item.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="send-extend-item">
+    <div class="item-icon">
+      <svg class="icon extend-click" aria-hidden="true">
+        <use xlink:href="#icon-wode"></use>
+      </svg>
+    </div>
+    <div class="text">{{this.text}}</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    mode: {
+      type: String
+    },
+    icon: {
+      type: String
+    },
+    text: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import '~@/common/style/theme.less';
+
+.send-extend-item {
+  width: 100%;
+  height: 100%;
+  .item-icon {
+    .icon {
+      // display: block;
+      width: 5.4rem;
+      height: 5.4rem;
+      // padding: 0 1.2rem;
+      background-color: #000;
+      fill: #999;
+    }
+  }
+  .text {
+    line-height: 2rem;
+    font-size: 1.2rem;
+    margin-top: 0.3rem;
+    color: @text-light;
+  }
+}
+</style>
---
+++ src/views/mainRoom/components/chat/send-gift.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="send-gift">
+    <send-extend-item
+      v-for="(item, index) in giftItems"
+      :key="index"
+      :mode="item.mode"
+      :icon="item.icon"
+      :text="item.text"
+    ></send-extend-item>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  components: {
+    'SendExtendItem': () => import('@/views/mainRoom/components/chat/send-extend-item')
+  },
+  data() {
+    return {
+      giftItems: [
+        {
+          icon: '#icon-wode',
+          text: '草莓蛋糕'
+        },
+        {
+          icon: '#icon-wode',
+          text: '草莓蛋糕'
+        },
+        {
+          icon: '#icon-wode',
+          text: '草莓蛋糕'
+        },
+        {
+          icon: '#icon-wode',
+          text: '草莓蛋糕'
+        },
+        {
+          icon: '#icon-wode',
+          text: '草莓蛋糕'
+        },
+        {
+          icon: '#icon-wode',
+          text: '草莓蛋糕'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.send-gift {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding: 2.4rem 1.8rem;
+  .send-extend-item {
+    height: 9.6rem;
+    flex-basis: 25%;
+    text-align: center;
+  }
+}
+</style>
