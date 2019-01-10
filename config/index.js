cad788b3e6004e224491934b0a02857a2cabbc39
Wang Tiangou <18132773191@189.cn>
2019-01-10 11:29:12 +0800

    config

--- config/index.js
+++ config/index.js
@@ -21,9 +21,9 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    // host: '192.168.8.106', // can be overwritten by process.env.HOST
-    port: 9000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    // host: 'localhost', // can be overwritten by process.env.HOST
+    host: '192.168.8.107', // can be overwritten by process.env.HOST
+    port: 8090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
--- src/common/js/share.js
+++ src/common/js/share.js
@@ -6,7 +6,7 @@ export const wxConfig = function(url) {
     const jssdk = res.data
     console.log(res.data)
     wx.config({
-      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
       appId: jssdk.appId, // 必填，公众号的唯一标识
       timestamp: parseInt(jssdk.timestamp),
       nonceStr: jssdk.nonceStr, // 必填，生成签名的时间戳
