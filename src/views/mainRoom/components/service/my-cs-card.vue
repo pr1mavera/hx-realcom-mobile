34efba2b7bdccc8129501aa68395974c5c64c1cd
Wang Tiangou <18132773191@189.cn>
2019-01-22 15:39:45 +0800

    -- debugger

--- src/views/mainRoom/components/service/my-cs-card.vue
+++ src/views/mainRoom/components/service/my-cs-card.vue
@@ -86,7 +86,6 @@
 
         // debugger
         const res = await removeCs(data)
-        debugger
         if (res.result.code === ERR_OK) {
           this.$emit('removeCs', this.csIndex)
           // console.log(JSON.stringify(res))
